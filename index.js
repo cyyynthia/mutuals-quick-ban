3931e123deeb60cc906d52ab2ca7d2f34a28cd58
Cynthia <cynthia@cynthia.dev>
2021-02-04 17:16:19 +0100

    Fix stuff

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 /*
- * Copyright (c) 2020 Bowser65
- * Licensed under the Open Software License version 3.0
+ * Copyright (c) 2021 Cynthia K. Rey, All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 const { Plugin } = require('powercord/entities');
@@ -33,7 +55,7 @@ module.exports = class MutualsQuickBan extends Plugin {
 
   _render (ogType, user, props) {
     const res = ogType(props);
-    const permsModule = getModule([ 'can', 'getState' ], false);
+    const permsModule = getModule([ 'can', 'isRoleHigher' ], false);
     const canKick = permsModule.can(Permissions.KICK_MEMBERS, props.guild);
     const canBan = permsModule.can(Permissions.BAN_MEMBERS, props.guild);
     if (canKick || canBan) {
